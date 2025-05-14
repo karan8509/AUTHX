a0eb103ac28aa6e2f2a68521c6cde250696b25b4
karan8509 <kj382542@gmail.com>
2025-05-14 14:56:35 +0330

    Initial commit

---
+++ Models/db.js
@@ -0,0 +1,10 @@
+// const mongoose = require('mongoose');
+// require("dotenv").config()
+// const DBURL = process.env.MONGO_CONN;
+
+// mongoose.connect(DBURL)
+//     .then(() => {
+//         console.log('MongoDB Connected...');
+//     }).catch((err) => {
+//         console.log('MongoDB Connection Error: ', err);
+//     })
